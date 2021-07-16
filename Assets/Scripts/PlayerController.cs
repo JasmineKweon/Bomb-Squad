74ef4e0f33de5ce8e1fa69688d3bd796b4aeb74a
JasmineKweon <jasmine.j.kweon@gmail.com>
2021-07-16 22:54:10 +0900

    Added Animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,15 +8,15 @@ public class PlayerController : MonoBehaviour
     float horizontal;
     float vertical;
     Rigidbody2D rigidbody2d;
-    //Animator animator;
-    //Vector2 lookDirection = new Vector2(0, -1);
+    Animator animator;
+    Vector2 lookDirection = new Vector2(0, -1);
     Vector2 move;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
-        //animator = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -34,24 +34,16 @@ public class PlayerController : MonoBehaviour
         }
 
         move = new Vector2(horizontal, vertical);
-        /*
+        
         if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
         {
             lookDirection.Set(move.x, move.y);
             lookDirection.Normalize();
         }
 
-        animator.SetFloat("LookX", lookDirection.x);
-        animator.SetFloat("LookY", lookDirection.y);
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
         animator.SetFloat("Speed", move.magnitude);
-
-        Vector2 position = transform.position;
-
-        position.x = position.x + speed * horizontal * Time.deltaTime;
-        position.y = position.y + speed * vertical * Time.deltaTime;
-
-        transform.position = position;
-        */
     }
 
     void FixedUpdate()

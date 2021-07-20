345217d56acbe0adb56a3228de801e095b5bb1a3
JasmineKweon <jasmine.j.kweon@gmail.com>
2021-07-20 22:51:05 +0900

    Explosion destroys boxes done

--- Assets/Scripts/ExplosionController.cs
+++ Assets/Scripts/ExplosionController.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class ExplosionController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
     // Start is called before the first frame update
     void Start()
     {
+        rigidbody2d = GetComponent<Rigidbody2D>();
         Destroy (gameObject, this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length); 
     }
 
@@ -14,5 +16,15 @@ public class ExplosionController : MonoBehaviour
     void Update()
     {
 
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {   
+        Debug.Log(other.gameObject.name);
+
+        if (other.gameObject.name.StartsWith("Medieval_props_free_10"))
+        {
+            Destroy(other.gameObject);
+        }
     }
 }

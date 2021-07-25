e3d8b9abf952224552b2149aef8a39b00140cb33
JasmineKweon <jasmine.j.kweon@gmail.com>
2021-07-25 18:24:21 +0900

    DropBomb on Grid

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -10,6 +10,8 @@ public class GridManager : MonoBehaviour
     public float x_Space, y_Space;
     public GameObject boxPrefab;
 
+    //public GameObject propPrefab;
+
     float x_Position;
     float y_Position;
 
@@ -19,32 +21,34 @@ public class GridManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+    /*
         for (int i = 0; i < columnLength * rowLength; i++)
         {
             column = i % columnLength;
             row = i / columnLength;
 
-            if (((column==1)&&(row==0))||((column==0)&&(row==1))||((column==columnLength-2)&&(row==0))||((column==columnLength-1)&&(row==1)))
+            if (((column == 0) && (row == 0)) || ((column == columnLength-1) && (row == 0)) || ((column == 1) && (row == 0)) || ((column == 0) && (row == 1)) || ((column == columnLength - 2) && (row == 0)) || ((column == columnLength - 1) && (row == 1)))
             {
                 continue;
             }
-
-            x_Position = x_Start + (x_Space * column);
-            y_Position = y_Start + (y_Space * row); 
-
-            Vector2 position = new Vector3(x_Position, y_Position, 0);
-        
-            if(Physics2D.Raycast(position, Vector2.up, 0.5f)!=true)
+            else
             {
-                Instantiate(boxPrefab, position, Quaternion.identity);
+                x_Position = x_Start + (x_Space * column);
+                y_Position = y_Start + (y_Space * row);
+
+                Instantiate(boxPrefab, new Vector3(x_Position, y_Position), Quaternion.identity);
             }
+
             
-            //Instantiate(boxPrefab, new Vector3(x_Position, y_Position), Quaternion.identity);
-        }
+            //if (Physics2D.Raycast(position, Vector2.up, 0.5f) != true)
+            //{
+                //Instantiate(boxPrefab, new Vector3(x_Position, y_Position), Quaternion.identity);            
+            //}
+        }*/
     }
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,21 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 1.5f;
+    public float speed = 2.0f;
     float horizontal;
     float vertical;
-    Rigidbody2D rigidbody2d;
+    //Rigidbody2D rigidbody2d;
+    BoxCollider2D boxCollider;
     Animator animator;
     Vector2 lookDirection = new Vector2(0, -1);
     Vector2 move;
+
+    Vector3 pos;
 
     public GameObject bombPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody2d = GetComponent<Rigidbody2D>();
+        //rigidbody2d = GetComponent<Rigidbody2D>();
+        boxCollider =GetComponent<BoxCollider2D>();
         animator = GetComponent<Animator>();
+        pos = transform.position;
     }
 
     // Update is called once per frame
@@ -35,8 +40,9 @@ public class PlayerController : MonoBehaviour
             vertical = Input.GetAxis("Player2-Vertical");
         }
 
+        //Animation
         move = new Vector2(horizontal, vertical);
-        
+
         if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
         {
             lookDirection.Set(move.x, move.y);
@@ -47,26 +53,55 @@ public class PlayerController : MonoBehaviour
         animator.SetFloat("Look Y", lookDirection.y);
         animator.SetFloat("Speed", move.magnitude);
 
-        if (Input.GetKeyDown(KeyCode.M))
+        //DropBomb
+        if (Input.GetKeyDown(KeyCode.M) && gameObject.name == "Player1")
         {
-            DropBomb("Player1");
+            DropBomb();
         }
-        if (Input.GetKeyDown(KeyCode.Z))
+        if (Input.GetKeyDown(KeyCode.Z) && gameObject.name == "Player2")
         {
-            DropBomb("Player2");
+            DropBomb();
         }
     }
 
     void FixedUpdate()
     {
+        /*
         Vector2 position = rigidbody2d.position;
         position = position + speed * move * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
+        */
+
+        boxCollider.enabled = false;
+        RaycastHit2D hitup = Physics2D.Raycast(transform.position, Vector2.up, 1.0f);
+        RaycastHit2D hitdown = Physics2D.Raycast(transform.position, Vector2.down, 1.0f);
+        RaycastHit2D hitright = Physics2D.Raycast(transform.position, Vector2.right, 1.0f);
+        RaycastHit2D hitleft = Physics2D.Raycast(transform.position, Vector2.left, 1.0f);
+        boxCollider.enabled = true;
+
+        if (horizontal < 0 && transform.position == pos && hitleft.collider == null) 
+        {        // Left
+            pos += Vector3.left;
+        }
+        if (horizontal > 0 && transform.position == pos && hitright.collider == null) 
+        {        // Right
+            pos += Vector3.right;
+        }
+        if (vertical > 0 && transform.position == pos && hitup.collider == null) 
+        {        // Up
+            pos += Vector3.up;
+        }
+        if (vertical < 0 && transform.position == pos && hitdown.collider == null) 
+        {        // Down
+            pos += Vector3.down;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);    // Move there
     }
 
-    void DropBomb(string name)
+    void DropBomb()
     {
-        GameObject bombObject = Instantiate(bombPrefab, GameObject.Find(name).transform.position, Quaternion.identity);
+        Instantiate(bombPrefab, pos - new Vector3(0,0.5f) , Quaternion.identity);        
     }
 }
